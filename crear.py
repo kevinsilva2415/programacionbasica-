2e6f9570a0f690c424f2eecc7a579cc712d6a1c7
kevinsilva2415 <100029501+kevinsilva2415@users.noreply.github.com>
2022-02-19 08:18:01 -0500

    Add files via upload

---
+++ crear.py
@@ -0,0 +1,7 @@
+nombre=input("cual es su nombre: ")
+num_1=int(input("ingrese numero: "))
+
+for i in range(num_1):
+    print(nombre)
+
+
---
+++ otro.py
@@ -0,0 +1,8 @@
+print("#"*50)
+print("tabla de multiplicar")
+print("#"*50)
+
+numero=int(input("ingrese un numero: "))
+
+for i in range(11):
+    print(numero,"x",i,"=",numero*i)
