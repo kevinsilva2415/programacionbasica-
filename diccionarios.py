46e0cbd232c4e571ac5fdd45ff3508bdb5a842b1
kevinsilva2415 <100029501+kevinsilva2415@users.noreply.github.com>
2022-03-10 18:46:38 -0500

    Add files via upload

---
+++ diccionarios.py
@@ -0,0 +1,7 @@
+diccionario={"nombre":"kevin",
+             "edad":20,
+             "clases":["programacion","calculo","redes"],
+             "estatura":1.70}
+print(diccionario)
+print(diccionario["nombre"])
+print(diccionario["clases"][1])
